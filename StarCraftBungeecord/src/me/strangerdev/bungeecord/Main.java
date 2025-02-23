fd0c2d61c197e74dca8de49d62d95126beefdd99
SrPuti <68877482+SrPuti@users.noreply.github.com>
2025-02-22 22:16:14 -0300

    geração de palavras chaves para entrada máxima, entre letras para número aleatório, com sua coeficiente.

--- StarCraftBungeecord/src/me/strangerdev/bungeecord/Main.java
+++ StarCraftBungeecord/src/me/strangerdev/bungeecord/Main.java
@@ -18,20 +18,29 @@ public class Main extends Plugin {
 		BungeeCord.getInstance().getPluginManager().registerCommand(this, new BedWarsCommand());
 		BungeeCord.getInstance().getPluginManager().registerCommand(this, new SkywarsCommand());
 		BungeeCord.getInstance().getPluginManager().registerCommand(this, new MiniFullPvP());
-		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent("§aPlugin ativado!"));
+		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent("Â§aPlugin ativado!"));
 		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent(""));
-		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent("§7Bungeecord Utils.!"));
+		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent("Â§7Bungeecord Utils.!"));
 		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent(""));
 
 	}
 	public void onDisable() {
 		
 		
-		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent("§aPlugin desativado!"));
+		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent("Â§aPlugin desativado!"));
 		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent(""));
-		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent("§7Bungeecord Utils.!"));
+		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent("Â§7Bungeecord Utils.!"));
 		BungeeCord.getInstance().getConsole().sendMessage(new TextComponent(""));
 
 	}
+}
+ system.out.print("ola, mundo. testando sistema para funcionamento.")
+public String test(int max){
+  int random = Math.int(100)+1/max;
+  StringBuilder builder = new StringBuilder()
+  for (int i = 0;i < random;i++){
+      builder.append(String.concat(""+new String((char)i))
+  }
+  retorno builder.toString();
 }
 
